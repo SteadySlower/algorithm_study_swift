4e2fff54a2509a9f2c11b8a268d571dd44391760
JW Moon <moon910904@gmail.com>
2022-05-03 08:11:00 +0900

    5월 2일 알고리즘 문제 풀이

---
+++ algorithm_study/Problem solving/20220502.swift
@@ -0,0 +1,11 @@
+//
+//  20220502.swift
+//  algorithm_study
+//
+//  Created by JW Moon on 2022/05/03.
+//
+
+import Foundation
+
+let array = readLine()!.components(separatedBy: " ").map { Int($0)! }
+print(array[0] + array[1])
