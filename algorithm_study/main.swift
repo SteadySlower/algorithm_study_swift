4a8f1779d8ac0230dc79db3ca2a7e00f801e546d
teddy <teddy@akaai.kr>
2022-05-06 09:55:46 +0900

    20220506 문풀

--- algorithm_study/main.swift
+++ algorithm_study/main.swift
@@ -7,14 +7,3 @@
 
 import Foundation
 
-
-// 백준 10950 A + B - 3
-
-let T = Int(readLine()!)!
-
-for _ in 0..<T {
-    let inputs = readLine()!.components(separatedBy: " ").map { Int($0)! }
-    let a = inputs[0]
-    let b = inputs[1]
-    print(a + b)
-}
