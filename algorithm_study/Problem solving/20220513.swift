c4c9a4f271bcd245e3024d43458258c742987854
JW Moon <moon910904@gmail.com>
2022-05-14 22:54:59 +0900

    20220514 interim

--- algorithm_study/Problem solving/20220513.swift
+++ algorithm_study/Problem solving/20220513.swift
@@ -37,3 +37,22 @@
 //    print("\(n - i + 1)/\(i)")
 //}
 
+/*
+ 하루 밤에 A - B 미터만큼 올라간다.
+ 중요한 것은 마지막 하루를 어떻게 처리하느냐는 것이다!
+ */
+
+//let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+//
+//let A = input[0]
+//let B = input[1]
+//let V = input[2]
+//
+//var days = (V - A) / (A - B)
+//let rem = (V - A) % (A - B)
+//
+//if rem == 0 {
+//    print(days + 1)
+//} else {
+//    print(days + 2)
+//}
--- algorithm_study/main.swift
+++ algorithm_study/main.swift
@@ -5,22 +5,15 @@
 //  Created by JW Moon on 2022/05/02.
 //
 
-/*
- 하루 밤에 A - B 미터만큼 올라간다.
- 중요한 것은 마지막 하루를 어떻게 처리하느냐는 것이다!
- */
+let T = Int(readLine()!)!
 
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-
-let A = input[0]
-let B = input[1]
-let V = input[2]
-
-var days = (V - A) / (A - B)
-let rem = (V - A) % (A - B)
-
-if rem == 0 {
-    print(days + 1)
-} else {
-    print(days + 2)
+(0..<T).forEach { _ in
+    let inputs = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let H = inputs[0]
+    let W = inputs[1]
+    let n = inputs[2]
+    let h = (n - 1) / H + 1
+    let w = (n - 1) % 10
+    let wStirng = 
+    print("\(h)\(wString)")
 }
