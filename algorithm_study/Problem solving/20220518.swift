6f849e16c9d5c69b345bb1be0fbf62819ba9e49d
SteadySlower <moon910904@gmail.com>
2022-05-18 10:48:40 +0900

    20220518 문풀: main 파일 수정

--- algorithm_study/Problem solving/20220518.swift
+++ algorithm_study/Problem solving/20220518.swift
@@ -48,3 +48,30 @@
 //]
 //
 //print(distances.min()!)
+
+//
+//var xPositions = [Int]()
+//var yPositions = [Int]()
+//
+//(0..<3).forEach { _ in
+//    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+//    xPositions.append(input[0])
+//    yPositions.append(input[1])
+//}
+//
+//var targetX: Int = 0
+//var targetY: Int = 0
+//
+//xPositions.forEach { x in
+//    if xPositions.filter({ $0 == x }).count == 1 {
+//        targetX = x
+//    }
+//}
+//
+//yPositions.forEach { y in
+//    if yPositions.filter({ $0 == y }).count == 1 {
+//        targetY = y
+//    }
+//}
+//
+//print(targetX, targetY)
