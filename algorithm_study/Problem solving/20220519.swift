657103aa54dd76854a3b500b2efd37d543eeacd8
JW Moon <moon910904@gmail.com>
2022-05-19 18:15:43 +0900

    20220519 알고리즘 문제풀이

---
+++ algorithm_study/Problem solving/20220519.swift
@@ -0,0 +1,30 @@
+//
+//  20220519.swift
+//  algorithm_study
+//
+//  Created by JW Moon on 2022/05/19.
+//
+
+// 직각 삼각형
+//import Foundation
+//
+//while true {
+//    let inputs = readLine()!.split(separator: " ").map { Double(String($0))! }.sorted()
+//    let a = inputs[0], b = inputs[1], c = inputs[2]
+//
+//        // a, b, c가 0 0 0이면 탈출
+//    guard a + b + c != 0 else { break }
+//
+//    if pow(a, 2) + pow(b, 2) == pow(c, 2) {
+//        print("right")
+//    } else {
+//        print("wrong")
+//    }
+//}
+
+// 택시 기하학
+
+//let R = Double(readLine()!)!
+//
+//print(R * R * Double.pi)
+//print(R * R * 2)
