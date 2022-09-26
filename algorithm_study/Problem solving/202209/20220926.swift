f074c7f65cc0a7b249f3a334990183d3e2744eb6
JW Moon <moon910904@gmail.com>
2022-09-26 10:25:20 +0900

    20220926 알고리즘 문제 풀이

---
+++ algorithm_study/Problem solving/202209/20220926.swift
@@ -0,0 +1,14 @@
+//
+//  20220926.swift
+//  algorithm_study
+//
+//  Created by JW Moon on 2022/09/26.
+//
+
+//func solution(_ s:String) -> String {
+//
+//    let array = s.split(separator: " ").map { Int($0)! }
+//
+//    return "\(array.min()!) \(array.max()!)"
+//}
+
