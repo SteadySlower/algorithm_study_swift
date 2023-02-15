37dda01ec1c530975790a8624181025eecafa5bb
JW Moon <moon910904@gmail.com>
2023-02-15 23:09:45 +0900

    20230215 알고리즘 문제풀이

---
+++ algorithm_study/Problem solving/202302/20230215.swift
@@ -0,0 +1,29 @@
+//
+//  20230215.swift
+//  algorithm_study
+//
+//  Created by JW Moon on 2023/02/15.
+//
+
+//func gcd(_ a: Int, _ b: Int) -> Int {
+//    if b == 0 {
+//        return a
+//    } else {
+//        return gcd(b, a % b)
+//    }
+//}
+//
+//func solution(_ w:Int, _ h:Int) -> Int64 {
+//
+//    func gcd(_ a: Int, _ b: Int) -> Int {
+//        if b == 0 {
+//            return a
+//        } else {
+//            return gcd(b, a % b)
+//        }
+//    }
+//
+//    return Int64(w * h - (w + h - gcd(w, h)))
+//}
+//
+//print(solution(8, 12))
