fdf1efc9db2bc90ff7764530ed88bbee13250ec8
JW Moon <moon910904@gmail.com>
2023-03-22 21:36:30 +0900

    20230322

---
+++ algorithm_study/Problem solving/202303/20230322.swift
@@ -0,0 +1,21 @@
+//
+//  20230322.swift
+//  algorithm_study
+//
+//  Created by JW Moon on 2023/03/22.
+//
+
+//import Foundation
+//
+//func solution(_ k:Int, _ d:Int) -> Int64 {
+//    var ans = 0
+//
+//    for x in stride(from: 0, to: d + 1, by: k) {
+//        let y = Int(sqrt(Double(d * d - x * x)))
+//        ans += y / k + 1
+//    }
+//
+//    return Int64(ans)
+//}
+//
+//print(solution(2, 4))
