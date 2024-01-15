aba56e672d6dd6379b28b99a251d4c8658fefa21
JW Moon <moon910904@gmail.com>
2024-01-15 21:01:36 +0900

    20240115

--- algorithm_study/Problem solving/202401/20240115.swift
+++ algorithm_study/Problem solving/202401/20240115.swift
@@ -5,4 +5,28 @@
 //  Created by JW Moon on 1/15/24.
 //
 
-import Foundation
+//class Solution {
+//    func trap(_ height: [Int]) -> Int {
+//        if height.isEmpty { return 0 }
+//
+//        var water = 0
+//        var left = 0, right = height.count - 1
+//        var leftMax = height[left], rightMax = height[right]
+//
+//        while left < right {
+//            if height[left] <= height[right] {
+//                water += leftMax - height[left]
+//                left += 1
+//                leftMax = max(height[left], leftMax)
+//            } else {
+//                water += rightMax - height[right]
+//                right -= 1
+//                rightMax = max(height[right], rightMax)
+//            }
+//        }
+//
+//        return water
+//    }
+//}
+//
+//print(Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1]))
