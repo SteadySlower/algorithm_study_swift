a6431ef676aecc625e6b0aa0d9ed99a77de9d061
JW Moon <moon910904@gmail.com>
2024-01-23 18:45:58 +0900

    20240123

--- algorithm_study/Problem solving/202401/20240123.swift
+++ algorithm_study/Problem solving/202401/20240123.swift
@@ -5,4 +5,21 @@
 //  Created by JW Moon on 1/23/24.
 //
 
-import Foundation
+// [7,1,5,3,6,4]
+// [-6, 4, -2, 3, -2]
+
+//class Solution {
+//    func maxProfit(_ prices: [Int]) -> Int {
+//        var minPrice = prices[0]
+//        var profit = 0
+//
+//        for price in prices {
+//            profit = max(profit, price - minPrice)
+//            minPrice = min(minPrice, price)
+//        }
+//
+//        return profit
+//    }
+//}
+//
+//print(Solution().maxProfit([7,1,5,3,6,4]))
